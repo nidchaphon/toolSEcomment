62eb41acb6caee2551036c5f1250fb46101c65eb
Phattharasi <tunbak150239@gmail.com>
2016-12-15 13:12:05 +0700

    1st ToolSEComment

--- application/index.php
+++ application/index.php
@@ -17,7 +17,6 @@ echo $strHost;
 <html lang="th">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
